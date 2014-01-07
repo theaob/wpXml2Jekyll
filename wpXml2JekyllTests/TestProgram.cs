88534d6ccdc33232dd513262a5f0a77129c82e68
Onur Baykal <theaob@gmail.com>
2014-01-07 11:07:09 +0200

    added files

---
+++ wpXml2JekyllTests/TestProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace wpXml2JekyllTests
+{
+    [TestFixture]
+    class TestProgram
+    {
+        [Test]
+        public void TestProgramOutputs()
+        {
+            
+        }
+    }
+}
